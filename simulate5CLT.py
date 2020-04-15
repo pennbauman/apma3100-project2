5b6a8ebf0c56702f9f6cd81c1be765cd7c3e5354
Jennifer Long <j.alicia.long@gmail.com>
2020-04-14 22:53:23 -0400

    stub code for 5.2

---
+++ simulate5CLT.py
@@ -0,0 +1,53 @@
+# APMA 3100-006 S2020 Project 2
+# Group 7(2):
+#   Penn Bauman, David Hasani, Jennifer Long, Erick Tian
+#
+# Part 5: Central Limit Theorem
+
+from math import log, sqrt
+from rand18bit import rand18bit
+
+
+### General variables (settings of simulation)
+# Sample sizes
+n = [10, 30, 50, 100, 150, 250, 500, 1000]
+
+# NOTE: Need the 110 samples from part 4
+M_n = []
+
+# 2.1) Transform the given sample M_n into Z_n
+mean = 0 # Need to find
+stdev = 0 # Need to find
+
+def to_Z_n(n, m_n):
+    return (m_n - mean)/(stdev/sqrt(n))
+
+Z_n = []
+## For each M_n
+    # Call to_Z_n for size n
+    # Save result in Z_n array
+
+
+# 2.2) Estimate from the sample of Z_n the probabilities of seven events z
+
+# Phi values for each event z_j (from table)
+phi_z = {
+  −1.4: 0.0808,
+  −1.0: 0.1587,
+  −0.5: 0.3085,
+  0: 0.5,
+  0.5: 0.6915,
+  1.0: 0.8413,
+  1.4: 0.9192
+}
+
+## For each Z_n
+    # Iterate through z_j values in phi_z
+        # Find P[Z_n <= z]
+        # Find abs difference |F(z_j) - phi_z|
+    # Get max abs diff (MAD) out of the 7
+
+# 2.3) Evaluate the goodness-of-fit of the standard normal CDF 𝛷 to the
+# empirical CDF 𝐹_𝑛 in terms of the maximum absolute difference MAD_n
+
+# Done above in the for loop
